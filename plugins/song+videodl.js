5e5f35ecd77bfe648341606d599ace8f012a2d54
𝐂𝐘𝐁𝐄𝐓 𝐑𝐎𝐖𝐃 <Rowdykaryek@gmail.com>
2025-01-20 01:26:53 +0530

    Update song+videodl.js

--- plugins/song+videodl.js
+++ plugins/song+videodl.js
@@ -1,4 +1,4 @@
-const {cmd , commands} = require('../command')
+.const {cmd , commands} = require('../command')
 const fg = require('api-dylux')
 const fg = require('yt-search')
 
@@ -34,9 +34,10 @@ await conn.sendmessage(from,{image:{url: data.thumbnail},caption:desc},{quoted:m
 let down = await fg.yta(url)
 let downlodurl = down.dl_url
 
-//send audio message
+//send audio + document message
 await conn.sendmessage(from,{audio: {url:downlodurl},mimetype:"audio/mpeg"},{quoted})
-
+await conn.sendmessage(from,{audio: {url:downlodurl},mimetype:"video/mp4",fileName:data.title + ".mp3",caption:"MADE BY ROWD-MD 👽},{quoted})
+:}
 
 }catch(e){
 console.log(e)
@@ -44,3 +45,49 @@ reply(`${}`)
 }
 })
 
+
+
+
+//=================vedeo-dl===========
+
+cmd({
+    pattern: "viedo",
+    desc: "dawnlod videos",
+    category: "dawnlod",
+    filename: __filename
+},
+async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
+if(!q) return reply("please give me url or ti")
+const search = await yts(q)
+const data= search.videos[0]
+const url = data.url
+
+let desc = `
+🍭 ROWDY MD video Dawnlod 🍭
+
+title: ${data.title}
+description:  ${data.description}
+time:  ${data.timestamp}
+ago: ${data.ago}
+views: ${data.views}
+
+MADE BY ROWDY-MD 👽
+`
+await conn.sendmessage(from,{image:{url: data.thumbnail},caption:desc},{quoted:mek});
+
+//download video
+  
+let down = await fg.yta(url)
+let downlodurl = down.dl_url
+
+//send video + document message
+await conn.sendmessage(from,{video: {url:downlodurl},mimetype:"video/mp4"},{quoted})
+await conn.sendmessage(from,{video: {url:downlodurl},mimetype:"video/mp4",fileName:data.title + ".mp4",caption:"MADE BY ROWD-MD 👽"},{quoted})
+:}
+
+}catch(e){
+console.log(e)
+reply(`${}`)
+}
+})
