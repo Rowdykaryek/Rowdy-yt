07a524b6b5bdb173c081f778e6e3062c3b2a88f6
𝐂𝐘𝐁𝐄𝐓 𝐑𝐎𝐖𝐃 <Rowdykaryek@gmail.com>
2025-01-20 00:58:59 +0530

    Update song+videodl.js

--- plugins/song+videodl.js
+++ plugins/song+videodl.js
@@ -1,1 +1,46 @@
+const {cmd , commands} = require('../command')
+const fg = require('api-dylux')
+const fg = require('yt-search')
+
+
+cmd({
+    pattern: "song",
+    desc: "dawnlod song",
+    category: "dawnlod",
+    filename: __filename
+},
+async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
+if(!q) return reply("please give me url or ti")
+const search = await yts(q)
+const data= search.videos[0]
+const url = data.url
+
+let desc = `
+🍭 ROWDY MD SONG Dawnlod 🍭
+
+title: ${data.title}
+description:  ${data.description}
+time:  ${data.timestamp}
+ago: ${data.ago}
+views: ${data.views}
+
+MADE BY ROWDY-MD 👽
+`
+await conn.sendmessage(from,{image:{url: data.thumbnail},caption:desc},{quoted:mek});
+
+//download audio
+  
+let down = await fg.yta(url)
+let downlodurl = down.dl_url
+
+//send audio message
+await conn.sendmessage(from,{audio: {url:downlodurl},mimetype:"audio/mpeg"},{quoted})
+
+
+}catch(e){
+console.log(e)
+reply(`${}`)
+}
+})
 
